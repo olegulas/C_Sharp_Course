22ad3fe8b031d7e0c7296a83dc695632df6f53b8
olegulas <oleg_6699@mail.ru>
2022-11-27 02:38:41 +0300

    Done works

--- 1_lesson/1_3/Program.cs
+++ 1_lesson/1_3/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter digit N: ");
+int n = int.Parse(Console.ReadLine());
+int i_count = -n;
+
+while (i_count <= n)
+{
+    if (i_count == n) 
+    {
+        Console.Write(i_count); 
+        break;
+    }
+    Console.Write(i_count + ", ");
+    i_count ++;
+}
--- 1_lesson/1_3_hometask/Program.cs
+++ 1_lesson/1_3_hometask/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i_count = 1;
+Console.Write("Please, enter the digit N: ");
+int digN = int.Parse(Console.ReadLine());
+
+while (i_count <= digN)
+{
+    if (i_count % 2 == 0) Console.Write(i_count + " ");
+    i_count++;
+}
