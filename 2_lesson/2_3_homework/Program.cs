c017dee1423435b0f3d07c1b84f70af6127c68db
olegulas <oleg_6699@mail.ru>
2022-11-27 23:56:06 +0300

    Done 4 hometask

---
+++ 2_lesson/2_3_homework/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 4:
+Напишите программу, которая принимает на вход цифру,
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+bool isHoliday(int day)
+{
+    Console.WriteLine($"It's day - {day}");
+    if (day == 6 || day == 7) return true;
+    else return false;
+}
+
+Console.WriteLine(isHoliday(new Random().Next(1,8)));
