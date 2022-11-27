57fc11f17fc63c78dc533979f904f7e2206eb167
olegulas <oleg_6699@mail.ru>
2022-11-27 16:53:02 +0300

    Done demo example

---
+++ 2_lesson/2_0/Program.cs
@@ -0,0 +1,10 @@
+﻿int maxDigit(int arg)
+{
+    Console.WriteLine($"Random digit is {arg}");
+    if (arg / 10 >= arg % 10)
+        return arg / 10;
+    else
+        return arg % 10; 
+}
+
+Console.Write($"Max number - {maxDigit(new Random().Next(10,100))}");
