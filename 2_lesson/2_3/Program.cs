b620cc0386c43056505534f2b1eeacceee8491e4
olegulas <oleg_6699@mail.ru>
2022-11-27 18:55:40 +0300

    Done demo 4

---
+++ 2_lesson/2_3/Program.cs
@@ -0,0 +1,10 @@
+﻿ bool isMulpiply(int arg)
+{
+    Console.WriteLine($"Digit is {arg}:");
+    if ( (arg % 7 ==0) && (arg % 23 == 0) ) 
+        return true;
+    else 
+        return false;
+}
+
+Console.Write(isMulpiply(new Random().Next(1, 1001)));
