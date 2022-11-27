e2c01e3bf252fae713de4d8f00194d1464b605b4
olegulas <oleg_6699@mail.ru>
2022-11-28 01:00:50 +0300

    Done 3 hometask

---
+++ 2_lesson/2_2_homework/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 3:
+Напишите программу, которая выводит третью цифру
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+void secondNumberInDigit(int num)
+{
+int buf;
+Console.WriteLine($"The random number: {num}");
+    while (true)
+    {
+        buf = num % 10;
+        num = num / 10;
+        if (num > 9 && num < 100)
+        {
+            Console.Write($"Third digit - {buf}");
+            break;
+        }
+        else
+            if (num < 10)
+                {
+                    Console.Write("no third digit!");
+                    break;
+                }
+            continue;
+    }
+}
+
+secondNumberInDigit(new Random().Next(1, 10000));
