50ae5bb3571adf6b39ef199254fdee34a3373745
olegulas <oleg_6699@mail.ru>
2022-11-29 11:13:07 +0300

    Done 1 task in homework

---
+++ 3_lesson/3_0_homework/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+string isPalindrome(int n)
+{
+    int num = n;
+    int div = 0;
+    int rev = 0;
+    while (num > 0)
+    {
+        div = num % 10;
+        rev = 10 * rev + div;
+        num = num / 10;
+    }
+    
+    if (rev == n) 
+        return $"Digit {n} == revert({rev}) is palindrome";
+    else 
+        return $"Digit {n} != revert({rev}) is not palindrome";
+}
+
+Console.WriteLine(isPalindrome(23432));
