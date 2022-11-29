3c30e8ab89d9d4c5fad77e969d903cb43d029098
olegulas <oleg_6699@mail.ru>
2022-11-29 22:55:46 +0300

    Done 3 task in homework

---
+++ 3_lesson/3_2_homework/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+void rowCubes(int n)
+{
+    int count = 1;
+    while(count <= n)
+    {   
+        if (count == n)
+        {
+            Console.Write($"{Math.Pow(count, 3)} ");
+            return;
+        }
+        Console.Write($"{Math.Pow(count, 3)}, ");
+        count++;
+    }
+}
+
+rowCubes(3);
