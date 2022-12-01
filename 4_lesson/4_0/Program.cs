53e1bbee0572038b50be43b45f5bcc769bb49e71
olegulas <oleg_6699@mail.ru>
2022-12-01 20:51:35 +0300

    Done demo 1

---
+++ 4_lesson/4_0/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+int sumNums(int num)
+{
+    int result = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+
+Console.WriteLine(sumNums(int.Parse(Console.ReadLine())));
+
