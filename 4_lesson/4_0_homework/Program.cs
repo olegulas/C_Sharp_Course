499a892542424b86a32da6febfa845611f906098
olegulas <oleg_6699@mail.ru>
2022-12-02 12:44:55 +0300

    Done hometask 1

--- 4_lesson/4_0_homework/Program.cs
+++ 4_lesson/4_0_homework/Program.cs
@@ -3,3 +3,17 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+int Exponentiation(int value, int exp)
+{
+    int Result = 1;
+    for (int i = 1; i <= exp; i++)
+    {
+        Result *= value;
+    }
+    return Result;
+}
+Console.Write("Enter digit: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Enter exp: ");
+int expon = int.Parse(Console.ReadLine());
+Console.Write($"Digit {num} in pow {expon} = {Exponentiation(num, expon)}");
