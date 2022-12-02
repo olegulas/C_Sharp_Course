e27093cd51edb2ab4b6850b832ba4fa1d5ea3046
olegulas <oleg_6699@mail.ru>
2022-12-02 12:55:31 +0300

    done hometask 2

--- 4_lesson/4_1_homework/Program.cs
+++ 4_lesson/4_1_homework/Program.cs
@@ -4,3 +4,17 @@
 82 -> 10
 9012 -> 12
 */
+int GetSumFromDigit(int num)
+{
+    int Result = 0;
+    for (; num > 0;)
+    {
+        Result += num % 10;
+        num = num / 10;
+    }
+    return Result;
+}
+
+Console.Write("Enter digit: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write($"{num} -> {GetSumFromDigit(num)}");
