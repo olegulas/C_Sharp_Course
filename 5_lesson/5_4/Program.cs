dfcb8c96edc15129d9f35b10632033e5d9faaf13
olegulas <oleg_6699@mail.ru>
2022-12-06 20:15:09 +0300

    correct 5 task

--- 5_lesson/5_4/Program.cs
+++ 5_lesson/5_4/Program.cs
@@ -4,12 +4,12 @@
 [1 2 3 4 5] -> 5 8 3
 [6 7 3 6] -> 36 21 */
 
-void Print(int[] arr)
+void Print(int[] arr, string msg)
 {   /*
         Функция печати массива arr[]
     */
     int size = arr.Length;
-
+    Console.WriteLine(msg);
     for (int i = 0; i < size; i++)
         Console.Write($"{arr[i]} ");
     Console.WriteLine();
@@ -48,20 +48,22 @@ int[] NewArr(int[] arr)
     // заполнение нового массива произведением зеркальных элементов
     for (int i = 0; i < to; i++)
     {
-        NewArr[i] = arr[i] * arr[arr.Length - 1 - i];   
+        NewArr[i] = arr[i] * arr[arr.Length - 1 - i];
     }
 
     // если массив с нечетным числом, выдираем серединку в новый
-    if (!chet)                      
+    if (!chet)
         NewArr[NewArr.Length - 1] = arr[to];
 
     return NewArr;
 }
-
+Console.Clear();
+Console.WriteLine("Введите через Enter: Размер массива, " +
+                 "Начало, Конец диапазона случайных чисел");
+                 
 int[] arr_1 = ArrayNums(Convert.ToInt32(Console.ReadLine()),
                         Convert.ToInt32(Console.ReadLine()),
                         Convert.ToInt32(Console.ReadLine()));
-Console.Write("Исходный массив: ");
-Print(arr_1);
-Console.Write("Новый массив: ");
-Print(NewArr(arr_1));
+
+Print(arr_1, "Исходный массив:");
+Print(NewArr(arr_1), "Новый массив: ");
