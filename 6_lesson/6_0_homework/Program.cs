0b0ac0ae6a5f8b6ff8cd4acf1e19ec7c52f52ed8
olegulas <oleg_6699@mail.ru>
2022-12-10 20:01:37 +0300

    done 2 hometask 6 lesson

---
+++ 6_lesson/6_0_homework/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+
+void CountingPosNumbers()
+{
+    int countPosNum = 0;
+    string digitalSeries = string.Empty;
+    while (true)
+    {
+        string bufNum = Console.ReadLine();
+        if (bufNum == "")
+            break;
+
+        digitalSeries += bufNum + " ";
+
+        if (int.Parse(bufNum) > 0)
+            countPosNum++;
+    }
+    Console.WriteLine($"positive numbers in entered array: {digitalSeries}=> {countPosNum}");
+}
+
+CountingPosNumbers();
