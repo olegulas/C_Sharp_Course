b0db0a63b2bb6cec20bdf65a68a561a6960681f9
olegulas <oleg_6699@mail.ru>
2022-12-10 18:44:58 +0300

    done 1 hometask 6 lesson

---
+++ 6_lesson/6_1_homework/Program.cs
@@ -0,0 +1,16 @@
+﻿/*Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)*/
+
+(double, double) PointCrossTwoLine(double b1, double k1, double b2, double k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    return (x, y);
+}
+
+(double x, double y) = (PointCrossTwoLine(double.Parse(Console.ReadLine()),
+                                          double.Parse(Console.ReadLine()), 
+                                          double.Parse(Console.ReadLine()),
+                                          double.Parse(Console.ReadLine())));
+Console.WriteLine($"({x:F1}, {y:F1})");
