9f64ec675c1df57368568fba9a0342352280a5f8
olegulas <oleg_6699@mail.ru>
2022-12-19 15:08:41 +0300

    Done hometask 1 in lesson 8

---
+++ 8_lesson/8_0_homework/Program.cs
@@ -0,0 +1,78 @@
+﻿/* Задача 1: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2 */
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+}
+
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+
+    return arr;
+}
+
+
+void BubbleSortDECarr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        bool isSort = false;
+        int count = 0;
+        while (!isSort)
+        {
+            count = 0;
+            for (int j = 0; j < arr.GetLength(1) - 1; j++)
+            {
+                if (arr[i, j] < arr[i, j + 1])
+                {
+                    int t = arr[i, j + 1];
+                    arr[i, j + 1] = arr[i, j];
+                    arr[i, j] = t;
+                    count++;
+                }
+            }
+            if (count > 0)
+                isSort = false;
+            else
+                isSort = true;
+        }
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column,
+                        int.Parse(Console.ReadLine()),
+                        int.Parse(Console.ReadLine()));
+Console.WriteLine("Orig array:");
+Print(arr_1);
+BubbleSortDECarr(arr_1);
+Console.WriteLine("Sort array:");
+Print(arr_1);
