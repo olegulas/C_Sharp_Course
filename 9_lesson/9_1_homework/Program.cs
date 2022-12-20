a25fe9334db70933297c58ec9355246c529b5bf9
olegulas <oleg_6699@mail.ru>
2022-12-20 13:45:09 +0300

    Done hometask 2 in lesson 9

---
+++ 9_lesson/9_1_homework/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int SumInterval2Digit(int value1, int value2)
+{
+    if (value1 == value2) return value1;
+    return SumInterval2Digit(value1 + 1, value2) + value1;
+}
+
+Console.Write("Enter the start digit of interval: ");
+int start = int.Parse(Console.ReadLine());
+Console.Write("Enter the last digit of interval: ");
+int last = int.Parse(Console.ReadLine());
+Console.Write($"The amount of the interval digits ({start}..{last}) = {SumInterval2Digit(start, last)}");
