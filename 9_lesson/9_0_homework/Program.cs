a9615d46273f466d52d41885987ffeadcbc02280
olegulas <oleg_6699@mail.ru>
2022-12-20 17:58:18 +0300

    Done hometask 1 in lesson 9

---
+++ 9_lesson/9_0_homework/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Задача 1: Задайте значение N. Напишите программу, которая выведет все четные натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "4, 2"
+N = 8 -> "4, 6, 8" */
+
+string SeriesEvenNumbers(int value)
+{
+    if (value < 2)
+        return "Error, BAN";
+    else if (value % 2 != 0)
+        return SeriesEvenNumbers(value - 1);
+    else if (value == 2)
+        return "2";
+    return $"{value}, {SeriesEvenNumbers(value - 1)}";
+}
+
+Console.Write("Enter the digit: ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine(SeriesEvenNumbers(n));
